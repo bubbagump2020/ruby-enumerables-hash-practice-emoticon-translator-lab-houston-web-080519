d8ee6cd26c7064213cfa79af0cb02697387c59fb
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-23 18:09:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,15 +2,11 @@
 require 'yaml'
 def load_library(ruby_file)
   ruby_file_hash = {
-    :get_meaning => {ruby_file[]}
+    :get_meaning => {}
+    :get_emoticon => {}
   }
   
-  ruby_file_hash.each do |key|
-    key = ruby_file
-    if ruby_file_hash[:get_meaning] += 1
-      ruby_file_hash[:get_meaning][key]
-    end-
-  end
+end
   
   return ruby_file_hash
 end
