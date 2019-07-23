60507dbd32738f3a07a5126364f88d74a564ad4d
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-23 19:47:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,15 @@
 # require modules here
 require 'yaml'
 def load_library(ruby_file)
+  ruby_file_hash = {}
+  
   ruby_file.each do |key|
-    
+    ruby_file.dig(key)
+    if ruby_file_hash[:key]
+      ruby_file_hash[:key] += 1
+    else
+      ruby_file_hash[:key] = 1
+    end
   
   return ruby_file_hash
 end
