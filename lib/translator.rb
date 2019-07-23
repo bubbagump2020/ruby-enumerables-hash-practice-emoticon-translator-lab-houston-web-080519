31d1e32297645ec3ac01ee03d687d88f62a4406d
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-23 18:08:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,6 +5,11 @@ def load_library(ruby_file)
     :get_meaning => {ruby_file[]}
   }
   
+  ruby_file_hash.each do |key|
+    key = ruby_file
+    if ruby_file_hash[:get_meaning] += 1
+      ruby_file_hash[:get_meaning][key]
+  end
   
   return ruby_file_hash
 end
